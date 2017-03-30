f564758a4888f6b433a0b6e972e33189ef9823b6
Snolting <s.l.nolting@gmail.com>
2017-03-30 14:39:05 -0400

    Health check responsive header for survey

--- html/sophity-top-bar.php
+++ html/sophity-top-bar.php
@@ -1,8 +1,12 @@
 <?php
   echo "
-    <div class='hc-header'>
+    <div class='hc-header card-fixed'>
       <div class='hc-header-image'>
         <img src='http://www.sophity.com/wp-content/uploads/2016/07/sophity.png'>
+        </div>
+      <div class='hc-header-titles'>
+        <h1>Services Success Model</h1>
+        <h2>Health Check</h2>
       </div>
     </div>
   ";
